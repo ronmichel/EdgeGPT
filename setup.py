2e36d9ccde7803dc61e6e96ed6e33b9f74bf064c
bigbyto <bigbyto@gmail.com>
2024-04-21 02:34:11 +0800

    chore: 1.0.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if not PATH.exists():
 
 setup(
     name="EdgeGPT-fork",
-    version="1.0.1",
+    version="1.0.2",
     license="The Unlicense",
     author="Antonio Cheong,Bigbyto",
     author_email="acheong@student.dalat.org,bigbyto@gmail.com",
